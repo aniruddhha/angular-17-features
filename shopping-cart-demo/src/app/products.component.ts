4d914ace791d1aa6ecb45182314d74c74d3504c6
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-01-25 12:21:05 +0530

    pagination

--- shopping-cart-demo/src/app/products.component.ts
+++ shopping-cart-demo/src/app/products.component.ts
@@ -7,17 +7,28 @@ import {MatCardModule} from '@angular/material/card';
 import { Product } from './domains';
 import { RouterModule } from '@angular/router';
 
+import {PageEvent, MatPaginatorModule} from '@angular/material/paginator';
+
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [MatGridListModule, MatCardModule, MatButtonModule, RouterModule],
+  imports: [MatGridListModule, MatCardModule, MatButtonModule, RouterModule, MatPaginatorModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
 
   products ?: Array<Product>
+  length = 50;
+  pageSize = 5;
+  pageIndex = 0;
+  pageSizeOptions = [5, 10, 25];
+
+  hidePageSize = false;
+  showPageSizeOptions = true;
+  showFirstLastButtons = true;
+  disabled = false;
 
   constructor(
     private service : ShoppingdataService
@@ -27,10 +38,17 @@ export class ProductsComponent {
 
   ngOnInit() {
     this.products = this.service.fakeProducts()
+    this.length = this.service.prods.length
   }
 
   addToCart(product: Product) {
     this.service.addToCart(product)
+  }
+
+  handlePageEvent(e: PageEvent) {
+   console.log(e)
+
+   this.products = this.service.fakeProducts(e.pageIndex, 5)
   }
 
 }
